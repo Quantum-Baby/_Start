6f2fc3893c39fd1d85ca82a15274cf0b6973ff2d
Tarun <114307430+Quantum-Baby@users.noreply.github.com>
2022-10-02 22:44:23 +0530

    Delete 04_with.py

--- 04_with.py
+++
@@ -1,9 +0,0 @@
- ## With statement 
-'''
-The best way to open and close the file automaticaly is the with statement 
-with open("text.txt") as f :
-f.read()
-'''
-with open("another.txt", 'r') as f :
-    a = f.read()
-print(a)
