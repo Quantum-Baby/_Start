4ab7f5d556f7f1bf1de94550815c714202b01e58
Hate_Me <114307430+Quantum-Baby@users.noreply.github.com>
2023-01-07 23:01:07 +0530

    Create _Lambda_Function.py

---
+++ _Lambda_Function.py
@@ -0,0 +1,23 @@
+# Anonymous Functions | Lambda Functions
+
+# Lambda Functions -> Eg. - Multiplication 
+
+f = lambda a : a*a
+
+result = f(6)
+print(result)
+
+
+# Lambda Functions -> Eg. - Add
+f = lambda a,b : a+b
+
+result = f(6,5)
+print(result)
+
+
+# def Function
+def square(a):
+    return a*a
+
+result = square(5)
+print(result)
