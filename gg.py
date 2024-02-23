3683e4b4d823cff3033a126367f8d0e60d1eb6bb
Hate_Me <114307430+Quantum-Baby@users.noreply.github.com>
2024-02-23 13:49:59 +0530

    Update gg.py

--- gg.py
+++ gg.py
@@ -4,5 +4,8 @@ putput = 3.0
 Automatically convert the 1 which is int valu to the float value and add to the 2.0
 
 Explicit conversion 
-We are conver the data tupe as we wanted to 
+We are convert the data tupe as we wanted to
+example :
+num = 1 + int(2.0)
+output = 3
  
