946e12e7b32f1f79b73cf41a22fdc9bd57fbd2ff
Hate_Me <114307430+Quantum-Baby@users.noreply.github.com>
2024-02-23 13:48:47 +0530

    Create gg.py

---
+++ gg.py
@@ -0,0 +1,8 @@
+# Implicit conversion : Automatically covert the output
+example - 1 + 2.0
+putput = 3.0
+Automatically convert the 1 which is int valu to the float value and add to the 2.0
+
+Explicit conversion 
+We are conver the data tupe as we wanted to 
+ 
